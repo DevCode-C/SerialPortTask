7fbba0d5896a106b4d2328f4dfecf8a7b9e08fab
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-05-24 18:56:13 -0500

    Serial port conections first part

--- app/app_msps.c
+++ app/app_msps.c
@@ -1,8 +1,27 @@
 #include "stm32f0xx.h"
+#include "stm32f0xx_hal.h"
 #include <stdint.h>
 #include "app_bsp.h"
+#include "stm32f0xx_hal_conf.h"
+#include "stm32f070xb.h"
 
 void HAL_MspInit( void )
 {
     
+    
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    __HAL_RCC_USART2_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitStructure.Pin  = GPIO_PIN_2 | GPIO_PIN_3;
+    GPIO_InitStructure.Mode     = GPIO_MODE_AF_PP;
+    GPIO_InitStructure.Pull     = GPIO_NOPULL;
+    GPIO_InitStructure.Speed    = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStructure.Alternate = GPIO_AF1_USART2;
+
+    HAL_GPIO_Init(GPIOA,&GPIO_InitStructure);
 }
--- app/main.c
+++ app/main.c
@@ -2,19 +2,33 @@
 #include <stdint.h>
 #include "app_bsp.h"
 #include "stm32f070xb.h"
-#include "stm32f0xx_hal_gpio.h"
+#include "stm32f0xx_hal_conf.h"
 #include "funciones.h"
 
 /**------------------------------------------------------------------------------------------------
 Brief.- Punto de entrada del programa
 -------------------------------------------------------------------------------------------------*/
-
+UART_HandleTypeDef UartHandle;
 int main( void )
 {
     HAL_Init( );
-    initTask();
-    task();
+    UartHandle.Instance             = USART2;
+    UartHandle.Init.BaudRate        = 9600;
+    UartHandle.Init.WordLength      = UART_WORDLENGTH_8B;
+    UartHandle.Init.StopBits        = UART_STOPBITS_1;
+    UartHandle.Init.Parity          = UART_PARITY_NONE;
+    UartHandle.Init.HwFlowCtl       = UART_HWCONTROL_NONE;
+    UartHandle.Init.Mode            = UART_MODE_TX_RX;
+    UartHandle.Init.OverSampling    = UART_OVERSAMPLING_16;
 
+    HAL_UART_Init(&UartHandle);
+
+    for (; ;)
+    {
+        HAL_UART_Transmit(&UartHandle,(uint8_t *)"Hola mundo\n\r",sizeof("Hola mundo\n\r")-1,100);
+        HAL_Delay(1000);
+    }
+    
     return 0u;
 }
 
