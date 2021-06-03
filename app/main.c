795b2d5eb8516a54590586646bc67ab886218e0d
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-06-03 03:03:01 -0500

    Task 2 ended

--- app/main.c
+++ app/main.c
@@ -1,13 +1,14 @@
 #include "stm32f0xx.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "app_bsp.h"
 #include "stm32f070xb.h"
 #include "stm32f0xx_hal_conf.h"
 #include "string.h"
 #include "stm32f0xx_hal.h"
 #include "funciones.h"
-
+#include <ctype.h>
 /**------------------------------------------------------------------------------------------------
 Brief.- Punto de entrada del programa
 -------------------------------------------------------------------------------------------------*/
@@ -28,17 +29,16 @@ int main( void )
         if (status == SET)
         {
             status = RESET;
-            if (!memcmp("uno\r",RxBuffer,sizeof("uno\r")-1))
+            if (isdigit(RxBuffer[0]))
             {
                 HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_0);
+                HAL_UART_Transmit_IT(&UartHandle,(uint8_t*)"numero\r\n",sizeof("numero\r\n")-1);
             }
-            else if (!memcmp("dos\r",RxBuffer,sizeof("dos\r")-1))
+            else
             {
-                HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_1);
-            }
-            else if (!memcmp("tres\r",RxBuffer,sizeof("tres\r")-1))
-            {
-                HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_2);
+                HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_1);  
+                HAL_UART_Transmit_IT(&UartHandle,(uint8_t*)"caracter\r\n",sizeof("caracter\r\n")-1); 
+                
             }
         }
         
