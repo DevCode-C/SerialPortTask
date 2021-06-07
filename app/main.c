74073d0391dcb2c951425bfb97bf3db63c166a2c
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-06-07 01:57:36 -0500

    Example ended

--- app/main.c
+++ app/main.c
@@ -12,11 +12,13 @@
 /**------------------------------------------------------------------------------------------------
 Brief.- Punto de entrada del programa
 -------------------------------------------------------------------------------------------------*/
-
+UART_HandleTypeDef UartHandle;
 __IO ITStatus uartState = RESET;
 __IO ITStatus status = RESET;
-extern uint8_t RxBuffer[20];
+uint8_t RxBuffer[20];
+uint8_t RxByte;
 
+void UART_Init(void);
 
 int main( void )
 {
@@ -43,4 +45,36 @@ int main( void )
     return 0u;
 }
 
+void UART_Init(void)
+{
+    UartHandle.Instance             = USART2;
+    UartHandle.Init.BaudRate        = 9600;
+    UartHandle.Init.WordLength      = UART_WORDLENGTH_8B;
+    UartHandle.Init.StopBits        = UART_STOPBITS_1;
+    UartHandle.Init.Parity          = UART_PARITY_NONE;
+    UartHandle.Init.HwFlowCtl       = UART_HWCONTROL_NONE;
+    UartHandle.Init.Mode            = UART_MODE_TX_RX;
+    UartHandle.Init.OverSampling    = UART_OVERSAMPLING_16;
 
+    HAL_UART_Init(&UartHandle);
+    HAL_UART_Receive_IT(&UartHandle,&RxByte,1);
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+    uartState = SET;
+    memset(RxBuffer,0,sizeof(RxBuffer));
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    static uint32_t i = 0;
+    RxBuffer[i] = RxByte;
+    i++;
+    if(RxBuffer[i-1] == '\r')
+    {
+        status = SET;
+        i=0;
+    }
+    HAL_UART_Receive_IT(&UartHandle,&RxByte,1);
+}
